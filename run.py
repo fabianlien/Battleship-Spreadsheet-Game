348dc2768fda37076d98bbe5904247afda0f0ed4
Fabian Lien <fabian.lien@gmail.com>
2022-03-22 15:48:47 +0000

    add function outlines for initial user options and input validation

--- run.py
+++ run.py
@@ -14,4 +14,61 @@ SHEET = GSPREAD_CLIENT.open('battleship')
 PLAYER_BOARD = SHEET.worksheet('player_board')
 COMPUTER_BOARD = SHEET.worksheet('computer_board')
 
-print(PLAYER_BOARD.get_all_values())
+print('Welcome to "Battleship"')
+print("The classic World War 1 game, running directly in your terminal!\n")
+print('Type one of the following commands below and hit enter:\
+  "Rules",  "NewGame",  "Score",  "Forfeit"')
+input_command = input("Enter command: ")
+
+
+def rules():
+    """
+    Prints the rules for how to play the game.
+    """
+    print("insert rules string here...")
+
+
+def setup_newgame():
+    """
+    Request input for board size and ships and passes it to new game function.
+    """
+    print("newgame setup function")
+
+
+def print_current_score():
+    """
+    Print current scores to terminal.
+    """
+
+
+def forfeit_y_n():
+    """
+    Asks the user if they want to forfeit and either resets the game or
+    continues.
+    """
+    option = input("Are you sure you want to forfeit? Yes/No: ")
+    if list(option)[0].lower() == "y":
+        print("You lost.")
+    elif list(option)[0].lower() == "n":
+        print("Resume game:")
+    else:
+        raise ValueError(f"{option} is not a valid answer.")
+
+
+def validate_input(input_value):
+    """
+    makes all string data lower case and raises valueError if invalid data.
+    """
+    if input_value.lower() == "rules":
+        rules()
+    elif input_value.lower() == "newgame":
+        setup_newgame()
+    elif input_value.lower() == "score":
+        print_current_score()
+    elif input_value.lower() == "forfeit":
+        forfeit_y_n()
+    else:
+        raise ValueError(f"Unknown command {input_value}")
+
+
+validate_input(input_command)
