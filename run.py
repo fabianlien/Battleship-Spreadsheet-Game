626c25abd78cc9d7fb5da08896581d781685d8e1
Fabian Lien <fabian.lien@gmail.com>
2022-03-25 21:28:47 +0000

    attempt to fix computer positioning function

--- run.py
+++ run.py
@@ -222,6 +222,9 @@ def computer_pos_ships(setup_list):
     setup_grid(setup_list, COMPUTER)
     setup_grid(setup_list, HIT_MAP)
     ship_length = 4
+    
+    empty_grid = COMPUTER.get_all_values()
+    
 
     for i in range(3):
         r_c = random_coordinate(setup_list[0], setup_list[1], ship_length)
@@ -379,4 +382,48 @@ def main():
     main()
 
 
-main()
+def rand_int(val, ship_length):
+    """
+    test
+    """
+    #grid height length should be passed
+    grid_height = 6
+    grid_width = 6
+    if val == "x":
+        rand_x = random.randint(0, (grid_width - 1))
+        return rand_x
+    if val == "y":
+        rand_y = random.randint(1, (grid_height - ship_length))
+        return rand_y
+
+
+def test():
+    """
+    test function
+    """
+
+    setup_grid([6, 6, 1, 1, 2], COMPUTER)
+    setup_grid([6, 6, 1, 1, 2], HIT_MAP)
+
+    grid = COMPUTER.get_all_values()
+    print(rand_int("y", 4))
+    print(rand_int("x", 4))
+    grid[rand_int("y", 4)][rand_int("x", 4)] = "B"
+    pprint(grid)
+
+    col_occupied = True
+    while col_occupied is True:
+        rand_x = rand_int("x", 4)
+        if "B" not in grid[rand_x]:
+            grid[rand_int("y", 4)][rand_x] = "B"
+            col_occupied = False
+    pprint(grid)
+
+
+
+
+
+
+
+test()
+#main()
